78b0286130590a75c794d898e1cda34ba0ea5f3f
Step741 <rokkerstep@gmail.com>
2026-03-04 22:09:12 +0300

    Made edits
    Made edits to task 3

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -8,18 +8,23 @@ namespace HomeWork
         static void Main(string[] args)
         {
             //Задание 1
-            int[] numbers = new int[8] { 0, 1, 1, 2, 3, 5, 8, 13 };
+            int[] fNumbers = new int[8] { 0, 1, 1, 2, 3, 5, 8, 13 };
 
             //Задание 2
             string[] mounths = new string[12] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
 
             //Задание 3
-            int[,] matrix = new int[3, 3]
+            int[,] matrix = new int[3, 3];
+            int[] numbers = { 2, 3, 4 };
+
+            for (int lines = 0; lines < 3; lines++)
             {
-                {2,3,4},
-                {4,9,16},
-                {8,27,64}
-            };
+                for (int column = 0; column < 3; column++)
+                {
+                    matrix[lines, column] = (int)Math.Pow(numbers[column], lines + 1);
+                }
+            }
+            ;
 
             //Задание 4
             double[][] jArray = new double[3][];
