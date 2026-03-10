796a35674423d9f5c1d0b0bc93e2e614953a7bcd
Step741 <rokkerstep@gmail.com>
2026-03-10 16:14:29 +0300

    Working with classes
    Сreated a Unit class and tested its operation a bit.

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,17 @@
+﻿using Classes;
+
+class Program
+{
+    static void Main()
+    {
+        Unit unit = new Unit("Knight");
+
+        Console.WriteLine(unit.Name);
+        Console.WriteLine(unit.Health);
+
+        bool isDead = unit.SetDamage(20);
+
+        Console.WriteLine(unit.Health);
+        Console.WriteLine("Game over: " + isDead);
+    }
+}
---
+++ Classes/Classes/Unit.cs
@@ -0,0 +1,45 @@
+﻿namespace Classes
+{
+    public class Unit
+    {   //Открытые свойства
+        private float _health;
+
+        public string Name { get; }
+
+        public float Health => _health;
+
+        public int Damage { get; }
+
+        public float Armor { get; }
+
+        //Конструкторы
+        public Unit() : this(name: "Unknown Unit")
+        {
+        }
+
+        public Unit(string name)
+        {
+            Name = name;
+            _health = 100f;
+            Damage = 5;
+            Armor = 0.6f;
+        }
+
+        public float GetRealHealth() 
+        {
+            return Health * (1f + Armor);
+        }
+
+        public bool SetDamage(int value)
+        {
+            _health = _health - value * Armor;
+
+            if (_health <= 0f)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
